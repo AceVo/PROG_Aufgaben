dd01534c5083355c595c76e2edcc595753a6b701
SteveVoges <voges.steve@gmail.com>
2018-12-03 15:44:35 +0100

    Implementierung der Funktion der Klasse ValueElement

--- DVLStackQueue/src/implementierung/ValueElement.java
+++ DVLStackQueue/src/implementierung/ValueElement.java
@@ -5,4 +5,41 @@ import schnittstellen.IValueElement;
 public class ValueElement implements IValueElement
 {
 
+    private String name;
+    private int value;
+
+    public ValueElement(String name, int value)
+    {
+        setName(name);
+        setValue(value);
+    }
+
+    @Override
+    public String getName()
+    {
+        return name;
+    }
+
+    @Override
+    public void setName(String paramName)
+    {
+        this.name = paramName==null ? "nichts" : paramName;
+    }
+
+    @Override
+    public int getValue()
+    {
+        return value;
+    }
+
+    @Override
+    public void setValue(int paramValue)
+    {
+        this.value = paramValue;
+    }
+
+    public String toString()
+    {
+        return "Name des Elements: " + this.name + "; Wert des Elements: " + this.value;
+    }
 }
