6f322d02e0bc5c1c1f2c0c77a39027283732e80f
Steve Voges <voges.steve@gmail.com>
2018-12-24 17:22:42 +0100

    Implementierung der Schnittstelle IStack und des Rumpfes der Klasse Stack

---
+++ DVLStackQueue/src/implementierung/Stack.java
@@ -0,0 +1,42 @@
+package implementierung;
+
+import schnittstellen.IList;
+import schnittstellen.IStack;
+
+public class Stack implements IStack
+{
+    public IList getDVL()
+    {
+        return null;
+    }
+
+    public int getSize()
+    {
+        return 0;
+    }
+
+    public boolean isEmpty()
+    {
+        return false;
+    }
+
+    public boolean isFull()
+    {
+        return false;
+    }
+
+    public int pop()
+    {
+        return 0;
+    }
+
+    public void push(int value)
+    {
+
+    }
+
+    public int top()
+    {
+        return 0;
+    }
+}
