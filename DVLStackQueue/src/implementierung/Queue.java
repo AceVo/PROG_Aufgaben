0e088c62c8f6c448ec5e5f454928f3c595de35f2
Steve Voges <voges.steve@gmail.com>
2018-12-30 18:06:00 +0100

    Implementierung der Schnittstelle IQueue und des Rumpfes der Klasse Queue sowie der zugehörigen Klasse zum Testen der Schnittstelle

---
+++ DVLStackQueue/src/implementierung/Queue.java
@@ -0,0 +1,42 @@
+package implementierung;
+
+import schnittstellen.IList;
+import schnittstellen.IQueue;
+
+public class Queue implements IQueue
+{
+    public IList getDVL()
+    {
+        return null;
+    }
+
+    public int getSize()
+    {
+        return 0;
+    }
+
+    public boolean isEmpty()
+    {
+        return false;
+    }
+
+    public boolean isFull()
+    {
+        return false;
+    }
+
+    public int dequeue()
+    {
+        return 0;
+    }
+
+    public void enqueue(int value)
+    {
+
+    }
+
+    public int front()
+    {
+        return 0;
+    }
+}
---
+++ DVLStackQueue/tests/schnittstellen/IQueueTest.java
@@ -0,0 +1,42 @@
+package schnittstellen;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class IQueueTest
+{
+
+    @BeforeEach void setUp()
+    {
+    }
+
+    @Test void getDVL()
+    {
+    }
+
+    @Test void getSize()
+    {
+    }
+
+    @Test void isEmpty()
+    {
+    }
+
+    @Test void isFull()
+    {
+    }
+
+    @Test void dequeue()
+    {
+    }
+
+    @Test void enqueue()
+    {
+    }
+
+    @Test void front()
+    {
+    }
+}
