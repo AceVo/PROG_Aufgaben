20e5bcf98c902265976b5178735a0202e9549b3f
SteveVoges <voges.steve@gmail.com>
2019-01-04 15:20:31 +0100

    Initialer Commit mit vorgegebenen Klassen Rumpf und entsprechenden Tests

---
+++ SuchSortieralgorithmen/src/algorithm/Sort.java
@@ -0,0 +1,22 @@
+package algorithm;
+
+public class Sort
+{
+    public static void bubbleSort(int[] list){}
+
+    public static void bubbleSort(int[] list, int leftIdx, int rightIdx){}
+
+    public static void bubbleUp(int[] list, int leftIdx, int i){}
+
+    public static void quickSort(int[] list ){}
+
+    public static void quickSort(int[] list, int leftIdx, int ricghtIdx){}
+
+    public static void divide(int[] list, int leftIdx, int rightIdx){}
+
+    public static void swap(int[] list, int i, int j){}
+
+    public static String toString(int[] list, int start, int end){}
+
+    public static String toString(int[] list){}
+}
---
+++ SuchSortieralgorithmen/tests/algorithm/SortTest.java
@@ -0,0 +1,60 @@
+package algorithm;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SortTest
+{
+
+    @BeforeEach
+    void setUp()
+    {
+    }
+
+    @Test
+    void bubbleSort()
+    {
+    }
+
+    @Test
+    void bubbleSort1()
+    {
+    }
+
+    @Test
+    void bubbleUp()
+    {
+    }
+
+    @Test
+    void quickSort()
+    {
+    }
+
+    @Test
+    void quickSort1()
+    {
+    }
+
+    @Test
+    void divide()
+    {
+    }
+
+    @Test
+    void swap()
+    {
+    }
+
+    @Test
+    void toString()
+    {
+    }
+
+    @Test
+    void toString1()
+    {
+    }
+}
